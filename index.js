b1e6bfc9d77d57c24b18e49b4a0cce23b4451e24
Timilehin <timiboi14@gmail.com>
2024-06-06 22:05:50 +0100

    first commit

--- index.js
+++ index.js
@@ -7,3 +7,35 @@ window.addEventListener("scroll", function () {
     header.classList.remove("header-scrolled");
   }
 });
+function toggleOpen() {
+  const menu = document.querySelector(".ham-menu");
+  menu.style.right = 0;
+}
+
+function toggleClose() {
+  const menu = document.querySelector(".ham-menu");
+  menu.style.right = "-100%";
+}
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  nav: false,
+  dots: false,
+  margin: 10,
+  // autoplay: true,
+  // autoplayTimeout: 500,
+  // autoplayHoverPause: true,
+  // animateOut: "fadeOut",
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 8,
+    },
+  },
+});
